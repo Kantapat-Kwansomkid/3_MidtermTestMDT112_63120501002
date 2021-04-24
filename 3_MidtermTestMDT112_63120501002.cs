2a3eedde6ce362a489b0790f20a4ddaae76ff9e7
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-04-24 15:28:47 +0700

    Add files via upload

---
+++ 3_MidtermTestMDT112_63120501002.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _3_MidtermTestMDT112_63120501002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Input a string mode;
+            Input a float filmBackWidth;
+            filmBackWidth > 0;
+            Define floats(Length and fov);
+            mode = "fLength";
+            int i = 2, j = 1;
+            if (i == j)
+            {
+                Console.WriteLine("i == j");
+            }
+            else
+            {
+                Console.WriteLine("i != j");
+            }
+
+            Console.ReadLine();
+
+            int i = 4, j = 3;
+            if (i == j)
+            {
+                Console.WriteLine("i == j");
+            }
+            else if (i == 3)
+            {
+                Console.WriteLine("i == 3");
+            }
+            else
+            {
+                Console.WriteLine("i != j");
+            }
+            Console.ReadLine();
+
+            input a float fLength;
+            Input a float fov;
+            fLength > 0;
+            fov > 0.1 & fov < 6.28;
+            fov = 2 * atan(filmBackWidth / 2, fLength);
+            fLength = filmBackWidth / (2 * tan(fov / 2));
+            int i = 2, j = 1;
+            if (i == j)
+            {
+                Console.WriteLine("i == j");
+            }
+            else
+            {
+                Console.WriteLine("i != j");
+            }
+
+            Console.ReadLine();
+
+            int i = 4, j = 3;
+            if (i == j)
+            {
+                Console.WriteLine("i == j");
+            }
+            else if (i == 3)
+            {
+                Console.WriteLine("i == 3");
+            }
+            else
+            {
+                Console.WriteLine("i != j");
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("fLength =",{0});
+            Console.WriteLine("fov =",{0});
+            Console.ReadLine();
+        }
+    }
+}
